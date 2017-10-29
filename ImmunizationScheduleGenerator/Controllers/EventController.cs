7fe7a4df24f5a8ad16cfe922e8e4ddd8595cd949
tszo <tszo@microsoft.com>
2017-10-28 23:56:46 -0700

    update 3

--- ImmunizationScheduleGenerator/Controllers/EventController.cs
+++ ImmunizationScheduleGenerator/Controllers/EventController.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Web;
 using System.Web.Http;
@@ -10,20 +13,30 @@ namespace ImmunizationScheduleGenerator
 {
     public class EventController : ApiController
     {
-        private const string NewLine = "\r\n";
-
-        [HttpGet]
-        public string GenerateSchedule(string name, string birthday)
+        public HttpResponseMessage  GetPatientCalendarFile(string name, string birthday)
         {
-            ImmunizationScheduleGenerator generator = new ImmunizationScheduleGenerator();
-            DateTime dtBirthday = Convert.ToDateTime(birthday);
-            string icsContent = generator.ICSFileGenerator(name, dtBirthday);
-            return icsContent;
-            //return icsContent.Replace("\r\n", "");
+            var data = GenerateSchedule(name, birthday);
+            var stream = new MemoryStream();
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                writer.Write(data);
+                writer.Flush();
+                stream.Position = 0;
+            }
+
+            var result = new HttpResponseMessage(HttpStatusCode.OK);
+            result.Content = new ByteArrayContent(stream.ToArray());
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            result.Content.Headers.ContentDisposition =
+                new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment")
+                {
+                    FileName = name+birthday + ".ics" // super simplify file name for demo purpose
+                };
+
+            return result;
         }
 
-
-        // POST api/GenerateSchedule
+        // POST api/GenerateSchedule (for multiple patient in one call) 
         public void GenerateSchedule([FromBody] PatientModel model)
         {
             ImmunizationScheduleGenerator generator = new ImmunizationScheduleGenerator();
@@ -44,6 +57,14 @@ namespace ImmunizationScheduleGenerator
                     fs.Write(info, 0, info.Length);
                 }
             }
+        }
+
+        private string GenerateSchedule(string name, string birthday)
+        {
+            ImmunizationScheduleGenerator generator = new ImmunizationScheduleGenerator();
+            DateTime dtBirthday = Convert.ToDateTime(birthday);
+            string icsContent = generator.ICSFileGenerator(name, dtBirthday);
+            return icsContent;
         }
     }
 }
